a324ecbcdc65b196919f130b3738fbb022687cd9
danielOuattara <daniel.ouattara@gmx.com>
2023-07-31 13:34:18 +0200

    05 BiFunction

---
+++ src/main/java/org/daniel/_05_bifunctions/Main.java
@@ -0,0 +1,58 @@
+package org.daniel._05_bifunctions;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("John", Gender.MALE),
+                new Person("Jade", Gender.FEMALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Bob", Gender.MALE),
+                new Person("Aretha", Gender.FEMALE)
+        );
+
+        //-----------------------------------------------------------------------------
+        // declarative solution : how to find the number of FEMALE ? // --> stream + collection
+
+        System.out.println("--------------------- DECLARATIVE --------------------- ");
+
+        // a predicate, from the Java Documentation
+        Predicate<Person> personPredicate = person -> Gender.FEMALE.equals(person.gender);
+        List<Person> females2 = people.stream()
+                .filter(personPredicate)
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+
+
+        //-----------------------------------------------------------------------------
+
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+
+}
---
+++ src/main/java/org/daniel/_05_bifunctions/_Functions_and_BiFunctions.java
@@ -0,0 +1,69 @@
+package org.daniel._05_bifunctions;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/*______________________________________________________________________
+Code this :
+Function<T,R>
+Represents a function that accepts one argument and produces a result.
+_________________________________________________________________________ */
+public class _Functions_and_BiFunctions {
+    public static void main(String[] args) {
+
+        System.out.println("------------------------------------------------");
+
+        int increment = incrementByOneMethod(0);
+        System.out.println("increment = " + increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println("increment2 = " + increment2);
+
+        System.out.println("------------------------------------------------");
+
+        int multiply = multiplyBy10Function.apply(3);
+        System.out.println("multiply = " + multiply);
+
+        int incrementAndMultiplyFunction = incrementBy1AndThenMultiplyBy10Function.apply(4);
+        System.out.println("incrementAndMultiplyFunction = " + incrementAndMultiplyFunction);
+
+        System.out.println("------------------------------------------------");
+
+        int incrementByOneAndMultiplyBy10 = incrementByOneAndMultiplyBy10(2, 4);
+        System.out.println("incrementByOneAndMultiplyBy10 = " + incrementByOneAndMultiplyBy10);
+
+        int incrementAndMultiplyBiFunction = incrementBy1AndThenMultiplyBy10BiFunction.apply(2, 4);
+        System.out.println("incrementAndMultiplyBiFunction = " + incrementAndMultiplyBiFunction);
+
+
+    }
+
+    // method  + imperative
+    static int incrementByOneMethod(int number) {
+        return number + 1;
+    }
+
+    /* Functional: Function
+     *--------------------------------*/
+    static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    /* Chaining Functions
+    ----------------------------------*/
+    static Function<Integer, Integer> incrementBy1AndThenMultiplyBy10Function =
+            incrementByOneFunction.andThen(multiplyBy10Function);
+
+    /* Functional BiFunctions
+    ----------------------------------*/
+    static int incrementByOneAndMultiplyBy10(int numberToIncrement, int numberToMultiply) {
+        return (numberToIncrement + 1) * numberToMultiply;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementBy1AndThenMultiplyBy10BiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy10) -> (numberToIncrementByOne + 1) * numberToMultiplyBy10;
+
+    /*
+    ----------------------------------*/
+
+}
